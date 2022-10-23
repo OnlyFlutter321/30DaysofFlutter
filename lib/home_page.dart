87f10a64930e33d3739cda9fbde11905a7060609
apple <apple@apples-MacBook-Pro.local>
2022-10-23 00:45:01 -0700

    day3

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  String name = "thirty";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("jsdh"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("$days is $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Yes"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
